d7856751cea51db0449ec2389bb5ca5adbe9e314
wedersonf <wedymf@hotmail.com>
2021-01-08 00:09:33 -0300

    funcionalidades

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,6 +5,11 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -14,15 +19,46 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = this.transactions.reduce(
+      (accumulator, currentValue) => {
+        if (currentValue.type === 'income') {
+          accumulator.income += currentValue.value;
+        } else if (currentValue.type === 'outcome') {
+          accumulator.outcome += currentValue.value;
+        }
+
+        accumulator.total = accumulator.income - accumulator.outcome;
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransaction): Transaction {
+    const newTransaction = new Transaction({ title, value, type });
+
+    if (newTransaction.type === 'outcome') {
+      const { total } = this.getBalance();
+
+      if (value > total) {
+        throw Error('Balance cannot be less than the total');
+      }
+    }
+
+    this.transactions.push(newTransaction);
+
+    return newTransaction;
   }
 }
 
